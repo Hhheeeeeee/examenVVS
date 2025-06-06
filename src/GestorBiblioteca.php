41b2262d354111d1d02bf03f3e2067093d1f142f
Hajar <hajarboukhlik123@gmail.com>
2025-06-06 16:14:37 +0200

    [refactor] - Refactorizado instruccion prestar en GestorBiblioteca

--- src/GestorBiblioteca.php
+++ src/GestorBiblioteca.php
@@ -4,11 +4,22 @@ namespace Deg540\DockerPHPBoilerplate;
 
 class GestorBiblioteca
 {
+    private $libros = [];
 
-    public function gestionarBiblioteca(string $instruccion)
+    public function gestionarBiblioteca(string $instruccion) : ?string
     {
         $partes = explode(" ", $instruccion);
+        $instruccionPrincipal = $partes[0];
+        $titulo = $partes[1] ?? "";
         $cantidad = $partes[2] ?? 1;
-        return $partes[1]." x".$cantidad;
+        if ($instruccionPrincipal == "prestar"){
+            if (!isset($this->libros[$titulo])){
+                $this->libros[$titulo] = $cantidad;
+                return $partes[1]." x".$cantidad;
+
+            }
+        }
+
+        return null;
     }
 }
